f876cf229f8ef6961811e0d4e2409b21389c3547
Dunfan Lu <ldfrank533@outlook.com>
2022-01-13 02:03:34 +0000

    wip

--- src/examples/taichi4/main.ts
+++ src/examples/taichi4/main.ts
@@ -25,6 +25,31 @@ let taichiExample4 = async () => {
     let place = dense.insert_children(taichi.SNodeType.place);
     console.log(place)
 
+    place.dt = taichi.PrimitiveType.i32
+
+    let aot_builder = program.make_aot_module_builder(taichi.Arch.vulkan);
+    console.log(aot_builder)
+
+    let ir_builder = new taichi.IRBuilder()
+    console.log(ir_builder)
+
+    let zero = ir_builder.get_int32(0)
+    console.log(zero)
+
+    let n_stmt = ir_builder.get_int32(n)
+    console.log(n_stmt)
+
+    let loop = ir_builder.create_range_for(zero, n_stmt, 1, 0, 4, 0, false);
+    console.log(loop)
+    {
+      let loop_guard = ir_builder.get_range_loop_guard(loop);
+      console.log(loop_guard)
+      let index = ir_builder.get_loop_index(loop,0);
+      console.log(index)
+    //   auto *ptr = ir_builder.create_global_ptr(place, {index});
+    //   ir_builder.create_global_store(ptr, index);
+      loop_guard.delete()
+    }
     
 }
 
