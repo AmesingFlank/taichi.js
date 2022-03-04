9003f856a3b6575a32457ae8bc245eaffd6d97b5
Dunfan Lu <ldfrank533@outlook.com>
2022-03-04 19:51:55 +0000

    start working on new type system

--- src/frontend/Type.ts
+++ src/frontend/Type.ts
@@ -1,5 +1,6 @@
 
 import {nativeTaichi, NativeTaichiAny} from "../native/taichi/GetTaichi"
+import {error} from "../utils/Logging"
 
 enum PrimitiveType {
     i32 = "i32",
@@ -37,5 +38,65 @@ class Type {
         return !this.isScalar && this.numCols > 1;
     }
 }
+
+enum FrontendTypeCategory {
+    Scalar = "Scalar",
+    Vector = "Vector",
+    Matrix = "Matrix"
+}
+
+class FrontendType {
+    constructor(){
+
+    }
+
+    getCategory():FrontendTypeCategory{
+        error("calling getCategory from FrontendType base")
+        return FrontendTypeCategory.Scalar
+    }
+
+    isScalar(): boolean {
+        return this.getCategory() == FrontendTypeCategory.Scalar
+    }
+
+    isVector(): boolean {
+        return this.getCategory() == FrontendTypeCategory.Vector
+    }
+
+    isMatrix(): boolean {
+        return this.getCategory() == FrontendTypeCategory.Matrix
+    }
+
+    public equals(that: FrontendType): boolean{
+        error("calling equlas from FrontendType base")
+        return false
+    }
+}
+
+class ScalarType extends FrontendType {
+    constructor(primitiveType:PrimitiveType){
+        super()
+        this.primitiveType_ = primitiveType
+    }
+    
+    private primitiveType_:PrimitiveType
+
+    override getCategory():FrontendTypeCategory{
+        return FrontendTypeCategory.Scalar
+    }
+
+    getPrimitiveType() : PrimitiveType {
+        return this.primitiveType_;
+    }
+
+    public equals(that: FrontendType): boolean{
+        if(that.getCategory() != this.getCategory()){
+            return false;
+        }
+        return this.getPrimitiveType() === (that as ScalarType).getPrimitiveType()
+    }
+}
+
+
 
 export {Type, PrimitiveType, toNativePrimitiveType}
