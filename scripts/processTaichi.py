2e90680c1c764932a224f661548cb7b2cc16c73a
Dunfan Lu <ldfrank533@outlook.com>
2022-05-06 23:00:44 +0100

    a few fixes

--- scripts/processTaichi.py
+++ scripts/processTaichi.py
@@ -5,6 +5,9 @@ def main():
         code = f.read()
     code += "export {createTaichiModule}\n"
     code = code.replace("f(-1)","")
+    code = code.replace('require("fs")',"undefined")
+    code = code.replace('require("path")',"undefined")
+    code = code.replace('require("child_process")',"undefined")
     with open("src/native/taichi/taichi.js","w") as f:
         f.write(code)
 
