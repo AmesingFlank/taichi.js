8cc4820ebfcfa269e71eef158359f207afdfe825
Dunfan Lu <ldfrank533@outlook.com>
2022-05-07 00:23:20 +0100

    use rollup replace plugin to remove weird requires from the typescript library

--- rollup/rollup.config.dev.mjs
+++ rollup/rollup.config.dev.mjs
@@ -2,6 +2,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'src/taichi.ts',
@@ -22,5 +23,9 @@ export default {
     resolve({
       browser: true
     }),
+    replace({
+      'require("source-map-support").install()': '',
+      delimiters: ['', '']
+    })
   ]
 };
--- rollup/rollup.config.prod.mjs
+++ rollup/rollup.config.prod.mjs
@@ -3,6 +3,7 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'src/taichi.ts',
@@ -23,6 +24,10 @@ export default {
     resolve({
       browser: true
     }),
-    terser()
+    terser(),
+    replace({
+      'require("source-map-support").install()': '',
+      delimiters: ['', '']
+    })
   ]
 };
